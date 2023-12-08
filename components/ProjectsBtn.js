1e8d6e4d05d154af1e5bd29de11e8155ebd6ed82
Emiliano006 <emiliano.costabile@hotmail.com>
2023-12-08 20:03:50 -0300

    commit

--- components/ProjectsBtn.js
+++ components/ProjectsBtn.js
@@ -8,7 +8,7 @@ const ProjectsBtn = () => {
   return ( <div className="mx-auto xl:mx-0">
     <Link
     href={'/work'}
-    className="relative w-[185px] h-[185px] flex justify-center items-center bg-circleStar bg-cover bg-center bg-no-repeat group">
+    className="relative z-[9] w-[185px] h-[185px] flex justify-center items-center bg-circleStar bg-cover bg-center bg-no-repeat group">
       <Image
       src={"/rounded-text.png"}
       width={141}
