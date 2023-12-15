aa92844830778604a087385a3128c18834dbf579
Emiliano006 <emiliano.costabile@hotmail.com>
2023-12-15 20:04:03 -0300

    responsive

--- pages/about/index.js
+++ pages/about/index.js
@@ -194,7 +194,7 @@ const About = () => {
       );
       })}
     </div>
-    <div className="py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start ">
+    <div className="py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start mb-5">
       {aboutData[index].info.map((item, itemIndex)=> {
         return (
           <div
