f4185d7cd1619334d149fb78ca10df15bd11b8d2
Emiliano006 <emiliano.costabile@hotmail.com>
2024-02-03 20:45:16 -0300

    avatar

--- components/Avatar.js
+++ components/Avatar.js
@@ -2,14 +2,22 @@ import Image from "next/image";
 
 const Avatar = () => {
   return (
-  <div className="hidden xl:flex xl:max-w-none">
-    <Image
-    src={'/avatar.png'}
-    width={437}
-    height={478}
-    alt=""
-    className="translate-z-0 w-full h-full top-0" />
-  </div>
+    <div className="hidden xl:flex xl:max-w-none">
+      <style jsx>{`
+        @media (max-width: 768px) {
+          .small-avatar {
+            width: 200px; /* Tamaño deseado para pantallas más pequeñas */
+            height: auto; /* Para mantener la proporción original */
+          }
+        }
+      `}</style>
+      <Image
+        src={'/avatar.png'}
+        width={437}
+        height={478}
+        alt=""
+        className="translate-z-0 w-full h-full top-0 small-avatar" />
+    </div>
   );
 };
 
