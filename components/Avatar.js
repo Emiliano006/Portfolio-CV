683eab2e9944af99fc2eff1e9ec170397b2b5d70
Emiliano006 <emiliano.costabile@hotmail.com>
2024-02-03 21:04:28 -0300

    avatar

--- components/Avatar.js
+++ components/Avatar.js
@@ -2,14 +2,15 @@ import Image from "next/image";
 
 const Avatar = () => {
   return (
-  <div className="hidden xl:flex xl:max-w-none">
-    <Image
-    src={'/avatar.png'}
-    width={437}
-    height={478}
-    alt=""
-    className="translate-z-0 w-full h-full top-0" />
-  </div>
+    <div className="hidden xl:flex xl:max-w-none">
+      <Image
+        src={'/avatar.png'}
+        width={437}
+        height={478}
+        alt=""
+        className="translate-z-0 w-full h-full top-0 xl:w-auto xl:h-auto"
+      />
+    </div>
   );
 };
 
